9feeba7481785d75a6932b9bf180200923839fb6
Christopher Riley <betterth@gmail.com>
2020-12-10 21:01:41 -0500

    except now it actually works

--- day1/day1pt2.py
+++ day1/day1pt2.py
@@ -1,6 +1,8 @@
 # day 1 part 2
 
 import itertools
+from functools import reduce
+import operator
 
 def main():
     input_list = []
@@ -8,11 +10,11 @@ def main():
     with open('input1.txt', 'r') as inputtxt:
         for line in inputtxt:
             input_list.append(int(line.rstrip()))
-    # Process data list and determine match, assuming only 1 or first match
+    # 
     two_tuple = check(input_list,2)
     three_tuple = check(input_list,3)
     print(two_tuple," ",three_tuple)
-    print(answer(two_tuple)," ",answer(three_tuple))
+    print(product(two_tuple)," ",product(three_tuple))
 
 def check(input_list,r):
     result = itertools.combinations(input_list,r)
@@ -20,7 +22,7 @@ def check(input_list,r):
         if sum(iteration) == 2020:
             return iteration 
 
-def answer(iterable):
+def product(iterable):
     return reduce(operator.mul, iterable, 1)
 
 if __name__ == "__main__":
