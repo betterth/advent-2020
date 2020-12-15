156c46c9e61c3a1d9d245c2b20e917f72df109af
Christopher Riley <betterth@gmail.com>
2020-12-14 22:35:57 -0500

    finished pt2

--- day3/day3-1.py
+++ day3/day3-1.py
@@ -2,23 +2,33 @@
 
 #slope = right3 down1
 def main():
-    #get iterable of input
-    answer = 0
-    x_max = 30 #0bound
-    #discard first line, set x position to 3 (0 bound)
-    x_pos = 3
-    input = handle_input('input.txt')[1:] #list slicing to remove the first entry
-    #print(len(input[0]))
+    input = handle_input('input.txt')
+    slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+    answer = 1
+    for ele in slopes:
+        x = ele[0]
+        y = ele[1]
+        iteration_result = check_problem(input,x,y)
+        print(ele,iteration_result)
+        answer *= iteration_result
+    print(answer)
+
+def check_problem(map,slope_x,slope_y):
     hits = 0
-    #
-    for index, line in enumerate(input):
+    x_pos = 0
+    x_max = len(map[0])
+    for index, line in enumerate(map):
+        #move y position more maybe
+        if index % slope_y != 0:
+            continue
+        #check for hits
         if line[x_pos] == "#":
             hits += 1
-            print("Hit at ",x_pos,",",index+2," ",hits)
-        x_pos += 3    
+        #move x position
+        x_pos += slope_x    
         if x_pos >= x_max:
             x_pos -= x_max
-    print(hits)
+    return hits
 
 def handle_input(file):
     input_list = []
