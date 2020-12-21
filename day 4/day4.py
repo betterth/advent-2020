852aa670253e38f9dd1de69c4a4c27a8a3a009da
Christopher Riley <betterth@gmail.com>
2020-12-20 21:15:55 -0500

    fixed

--- day 4/day4.py
+++ day 4/day4.py
@@ -6,14 +6,14 @@ def main():
     passport_lines = []
     passport_list = []
     #collect multi-line input into passport groups
-    input = handle_input('input.txt')
+    input = handle_input('day 4/input.txt')
     #issue: doesn't process last element because last line isn't blank
     for line in input:
-        if line == '':
+        if not line == '':
+            passport_lines.append(line)
+        else:
             passport_list.append(" ".join(passport_lines).split(" "))
             passport_lines = []
-        else:
-            passport_lines.append(line)
     #process passport list
     for passport in passport_list:
         total += 1
@@ -37,27 +37,45 @@ def passport_is_valid(passport):
     #iyr
     iyr = passport.get("iyr")
     if not iyr: return False
+    if not is_int(iyr): return False
     if not 2020 >= int(iyr) >= 2010: return False
     #
     eyr = passport.get("eyr")
     if not eyr: return False
+    if not is_int(eyr): return False
     if not 2030 >= int(eyr) >= 2020: return False
     #   
     hgt = passport.get("hgt")
     if not hgt: return False
-    if hgt.find("cm"):
+    if hgt.find("cm") != -1:
         if not 193 >= int(hgt.replace("cm","")) >= 150: return False
-    elif hgt.find("in"):
+    elif hgt.find("in") != -1:
         if not 76 >= int(hgt.replace("in","")) >= 59: return False
+    else: return False
     #
-    if "hcl" not in passport:
-        return False    
-    if "ecl" not in passport:
-        return False    
-    if "pid" not in passport:
-        return False   
+    hcl = passport.get("hcl")
+    if not hcl: return False
+    if not hcl[0] == "#": return False
+    if not len(hcl[1:]) == 6: return False
+    if not is_int(hcl[1:],16): return False
+    #
+    ecl = passport.get("ecl")
+    if not ecl in ("amb","blu","brn","gry","grn","hzl","oth"): return False
+    #
+    pid = passport.get("pid")
+    if not pid: return False
+    if not pid.isnumeric(): return False
+    if not len(pid) == 9: return False
+    # 
     else:
         return True     
+
+def is_int(num,base=10):
+    try:
+        int(num,base)
+        return True
+    except ValueError:
+        return False
 
 def handle_input(file):
     input_list = []
